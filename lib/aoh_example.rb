0eec74054a77a240cb9830821993237114884995
Andrew D'Antonio <andrewdantonio10@gmail.com>
2020-01-23 22:44:59 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -11,7 +11,11 @@ def assembled_aoh
 end
 
 def literal_aoh
-  DON_G.sort,JOELLE_VD.sort,PAT_M.sort,KATE_G.sort,BRUCE_G.sort
+[{ name:  "Don Gately", occupation: "Live-in Staff" },
+{ name:  "Joelle van Dyne", occupation: "Radio Personality" },
+{ name:  "Pat Monteseian", occupation: "Staff" },
+{ name:  "Kate Gompert", occupation: "None" },
+{ name:  "Bruce Green", occupation: "Fan of Mildred" }]
 end
 
 def aoh_lookup(aoh, row, key)
